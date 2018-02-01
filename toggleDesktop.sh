bde1ae36f9fb07769d3e3fa3c2605fd544772d01
Andy Challis <andy.challis@capgemini.com>
2018-02-01 01:48:27 +0000

    Initial commit

---
+++ toggleDesktop.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# <bitbar.title>Toggle desktop</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author.github>ghandic</bitbar.author.github>
+# <bitbar.author>AndyChallis</bitbar.author>
+# <bitbar.desc>Hides/shows the desktop</bitbar.desc>
+
+
+function get_darkmode_state {
+state=`defaults read com.apple.finder CreateDesktop`
+}
+
+if [[ "$1" = "show" ]]; then
+  defaults write com.apple.finder CreateDesktop true; killall Finder
+fi
+
+if [[ "$1" = "hide" ]]; then
+  defaults write com.apple.finder CreateDesktop false;killall Finder
+fi
+
+
+get_darkmode_state;
+if "$state" is true; then
+	echo "🙉"
+	echo "---"
+	echo "Hide Desktop Icons| bash='$0' param1=hide terminal=false refresh=true";
+	exit
+else
+	echo "🙈"
+	echo "---"
+	echo "Show Desktop Icons| bash='$0' param1=show terminal=false refresh=true";
+	exit
+fi
